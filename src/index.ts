9366410236ba948b05c37bec34c263a41e2e121a
ringoXD <nep360i@gmail.com>
2024-04-30 16:38:43 +0900

    umm

--- src/index.ts
+++ src/index.ts
@@ -2,7 +2,11 @@ import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
 import loki from 'lokijs'
 
-const db =new loki('./db/loki.json',{ autosave: true, autoload: false, autoupdate: true })
+const db =new loki('./db/loki.json',{
+  autosave: true,
+  autoload: false, //uwu
+  autoupdate: true
+})
 
 const items = db.addCollection('test')
 
@@ -10,6 +14,7 @@ items.insert({a: 1, b:2})
 items.insert({a: 2, b:3})
 
 
+// need this
 try {
   db.saveDatabase()
   console.log('Saved')
