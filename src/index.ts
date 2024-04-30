d219389b118ed900d532d88cd11f733c1725734d
ringoXD <nep360i@gmail.com>
2024-04-30 16:21:14 +0900

    lokijs test uwu

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,24 @@
 import { serve } from '@hono/node-server'
 import { Hono } from 'hono'
+import loki from 'lokijs'
+
+const db =new loki('./db/loki.json')
+
+const items = db.addCollection('test')
+
+items.insert({a: 1, b:2})
+items.insert({a: 2, b:3})
+items.saveToDisk()
 
 const app = new Hono()
 
 app.get('/', (c) => {
   return c.text('Hello Hono!')
+})
+
+app.get('/test', (c) => {
+  const result = items.findOne({a: 1})
+  return c.text(result)
 })
 
 const port = 3150
