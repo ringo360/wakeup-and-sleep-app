52ded5280a4d0ce62d094c594bf529bda889037a
ringoXD <nep360i@gmail.com>
2024-05-30 08:01:00 +0000

    hmm

--- src/websv.ts
+++ src/websv.ts
@@ -28,18 +28,18 @@ app.post('/v1/login', async (c) => {
 app.get('/v1/user/acctoken', async (c) => {
   console.log('Fire')
   const body = await c.req.parseBody()
-  const { RefreshToken } = body
-  if (!RefreshToken) return c.json({"Error": "Body is invalid"})
-  console.log(RefreshToken)
+  const { token } = body
+  if (!token) return c.json({"Error": "Body is invalid"})
+  console.log(token)
   try {
-    await verify(RefreshToken as string, JWTSecret)
+    await verify(token as string, JWTSecret)
   } catch (e) {
     return c.json({
       "Error": "Invalid Token"
     })
   }
   console.log('Generating')
-  const res = await genAccToken(RefreshToken as string)
+  const res = await genAccToken(token as string)
   console.log(res)
   const isok = res[0]
   if (isok !== true) {
