cbca1f9f58ec07e4605e3cf533b83ed7001fd323
ringoXD <nep360i@gmail.com>
2024-07-09 07:11:10 +0000

    hmm

--- src/auth.ts
+++ src/auth.ts
@@ -89,10 +89,10 @@ auth.get('/info', async (c) => {
     const v_res = await verify(token, JWTSecret)
     console.log(v_res) //for dev
     let accT;
-    console.log(`[0] is ${v_res[0]}`)
-    console.log(`t: ${v_res[0].token}`)
+    console.log(`res is ${v_res}`)
+    console.log(`t: ${v_res.token}`)
     if (v_res) {
-      accT = verify(v_res[0].token, JWTSecret)
+      accT = verify(v_res.token, JWTSecret)
     }
     return c.json({
       'OK': 'Success!',
