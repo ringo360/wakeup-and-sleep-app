5bab328c58f083d8d4baf6d8e2083d2607281d1d
ringoXD <nep360i@gmail.com>
2024-08-22 07:43:29 +0000

    npm run init

---
+++ src/init.ts
@@ -0,0 +1,10 @@
+import { ConfigChecker } from "./config"
+import { initdb } from "./db"
+
+async function main() {
+    console.log('Initializing...')
+    await ConfigChecker()
+    console.log('Checked config')
+    await initdb()
+    console.log('[DB] OK')
+}
